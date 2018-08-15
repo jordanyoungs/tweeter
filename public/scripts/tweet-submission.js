86f0840bae9bf5e8b927904162ae41a85291460b
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-15 21:49:04 +0000

    Fixed bug where textarea didn't clear on successful submission of a tweet. Another bug: A single long string tweet looks very ugly and doesn't get wrapped into its container, its bleeds into the sides of the page

--- public/scripts/tweet-submission.js
+++ public/scripts/tweet-submission.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
-    const tweetContent = $(this).serialize();
-    //slice the "text=" from start of tweetContent and count length
-    const tweetLength = tweetContent.slice(5).length;
+    const tweetContent = $(this).find("textarea").val();
+    const tweetLength = tweetContent.length;
 
     //if tweet length is 0 or undefined
     if (!tweetLength) {
       alert("Tweet cannot be empty!");
+
     //if tweet length is over 140 characters
     } else if (tweetLength > 140) {
       alert("Tweet cannot exceed 140 characters!");
+
     } else {
-      $.post("/tweets", tweetContent);
+      const urlEncodedTweet = $(this).serialize();
+      $.post("/tweets", urlEncodedTweet);
+      //clear textarea
+      $(this).find("textarea").val('');
     }
   });
 });
