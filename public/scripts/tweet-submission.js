9346001fc4d214e2014a624e3cec4d8b6362590d
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-15 21:29:29 +0000

    Added error messages when an empty tweet is submitted or a tweet over 140 characters is submitted. Bug: submitting a tweet successfully does not clear the form.

--- public/scripts/tweet-submission.js
+++ public/scripts/tweet-submission.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
-
     const tweetContent = $(this).serialize();
-    $.post("/tweets", tweetContent);
+    //slice the "text=" from start of tweetContent and count length
+    const tweetLength = tweetContent.slice(5).length;
+
+    //if tweet length is 0 or undefined
+    if (!tweetLength) {
+      alert("Tweet cannot be empty!");
+    //if tweet length is over 140 characters
+    } else if (tweetLength > 140) {
+      alert("Tweet cannot exceed 140 characters!");
+    } else {
+      $.post("/tweets", tweetContent);
+    }
   });
 });
