851683a72b996f48b20da5f7f3bce24082649bbe
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-15 16:07:43 +0000

    Forgot about git for awhile. Current functionality: Fixed page header to top of page, styled compose tweet box, made character counter function as expected and turn red when negative, styled tweets including a hover state that display icons in bottom right, currently working on a function that creates a tweet element from the tweet data object.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(".new-tweet textarea").on("keyup", function() {
+    $tweet = $(this);
+
+    const remainingChars = 140 - $tweet.val().length;
+    const $counter = $tweet.siblings("footer").find(".counter");
+
+    $counter.html(remainingChars);
+    $counter.toggleClass("negative", remainingChars < 0);
+
+  });
+});
