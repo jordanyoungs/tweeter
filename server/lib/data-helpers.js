f24fd62fcbdef4dfdc39df054cf7ece158f43491
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-16 19:10:40 +0000

    Get tweets is now retrieving tweets from a mongo database.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,8 +1,5 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
@@ -17,11 +14,10 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+      db.collection("tweets").find().toArray( (err, array) => {
+        callback(err, array.sort(sortNewestFirst));
       });
     }
-
   };
 }
